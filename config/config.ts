f4ce81e11401c25d06f68996ff3281ed3cf9001d
StefanCikarevic <stefancikarevic1996@gmail.com>
2022-09-07 11:14:55 +0200

    Server Finish

---
+++ config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+    firebase: {
+        apiKey: 'AIzaSyBmkPr4VdJVqQTmHJmc7pn1hsUMpwC9_o0',
+        authDomain: 'blog-mern-app-acef8.firebaseapp.com',
+        projectId: 'blog-mern-app-acef8',
+        storageBucket: 'blog-mern-app-acef8.appspot.com',
+        messagingSenderId: '809473224632',
+        appId: '1:809473224632:web:4f99efe38908d89bb8694d'
+    },
+    server: {
+        url: 'http://localhost:1337'
+    }
+};
+
+export default config;
